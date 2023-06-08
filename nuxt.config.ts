c89766a1b05a1e93b2659eadbb35293cf288c5fb
alfredolandi97 <alfredo.landi@mail.polimi.it>
2023-06-08 15:31:11 +0200

    Edited nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr:true,
-    modules:["@nuxtjs/supabase"]
+    modules:["@nuxtjs/supabase"],
+    css: [
+        '~/assets/css/general.css'
+    ],
 })
