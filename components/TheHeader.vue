c50c19b3c3111135ffa3625fea1fce0f0df4b80c
Luca_Vallacqua <Luca.Vallacqua@mail.polimi.it>
2023-06-09 10:58:27 +0200

    init

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -9,26 +9,67 @@
                 <NuxtLink to = "/news">News</NuxtLink>
                 <NuxtLink to = "/contact">About us</NuxtLink>
             </nav>
+
+
+
         </header>
 </template>
 
 <style>
     header
     {
-        background-color: greenyellow;
+        background-color: #0652ff;
         padding: 0px 20px 0px 20px;
-        margin-bottom: 5px;
-        border-bottom: 5px solid brown;
-    
+        margin-bottom: 10px;
+        height: 130px;
         display: flex;
         justify-content: space-between;
         align-items: center;
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 0 65%);
+        position: fixed;
+        width:100%;
     }
 
     nav {
         display: flex;
         gap: 30px;
         font-size: 17pt;
-        font-weight: bold;
+       
     }
+    h1{
+        padding: 0px 0px 40px 0px;
+
+    }
+    nav a {
+    color: #FFFF33; /* Colore dei link normali */
+    text-decoration: none;
+  }
+
+
+
+  /* custom scrollbar, qui si regolano le dimensioni */
+::-webkit-scrollbar {
+  width: 30px;
+}
+
+
+/* questo è per lo sfondo della scrollbar*/
+::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+
+/* caratteristiche della scrollbar stessa*/
+::-webkit-scrollbar-thumb {
+  background-color: blue;
+  border-radius: 20px;
+  border: 6px solid transparent;
+  background-clip: content-box;
+}
+
+/* comportamento quando si va sopra la scrollbar*/
+::-webkit-scrollbar-thumb:hover {
+  background-color: #a8bbbf;
+}
+
+
 </style>
