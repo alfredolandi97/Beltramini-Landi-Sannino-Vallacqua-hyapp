31c700f6cac3745e448ea5be206163cda9351810
Luca_Vallacqua <Luca.Vallacqua@mail.polimi.it>
2023-06-15 10:48:52 +0200

    mod ad aboutUs

--- pages/about.vue
+++ pages/about.vue
@@ -1,74 +1,81 @@
 <template>
+<h1 class="titolo">ABOUT US</h1>
+<div>
 
-<div class="card">
-  <img src="path_to_your_image" alt="Image" class="image">
-  <span class="textDiv" >METAVERSE</span>
+    <p class="firstpar">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut sint eum minima. Eius illum qui quas reprehenderit dolorum earum corporis adipisci nemo optio beatae explicabo, id dicta sit a magnam!</p>
+
+    <div class="firstsec">
+        <img  class="firstimg" src="../assets/img/about/papa_francesco_02-696x557.jpg" alt="">
+
+        <p class="secondpar"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione itaque cupiditate dignissimos rem pariatur consectetur consequatur voluptate nihil. Dolorem tempora ipsam error facere obcaecati nesciunt eligendi sit, at corrupti rerum! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quas esse nihil deserunt consequuntur? Quos vitae sapiente consequatur quisquam autem minima illo aliquam temporibus, provident nostrum ab iure, quibusdam repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quos fugit rerum cumque doloremque, delectus accusantium in distinctio illum aperiam accusamus vero sequi odit voluptates veniam provident saepe animi repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem officia iure porro eos velit. Ducimus repellendus, quibusdam tempore odit veniam, dolor ratione exercitationem tenetur aliquam nulla fugiat sunt, voluptatem deserunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum explicabo porro quas, assumenda nulla aliquid odio tempore, doloremque quisquam incidunt beatae, voluptas dicta commodi. At atque ut quibusdam unde veniam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laborum quasi corporis maiores iure mollitia asperiores recusandae magni eos, obcaecati labore, quos sit cupiditate optio impedit dolores molestiae ducimus pariatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, harum maiores iste molestiae odit dolor, facere neque, libero impedit sunt quisquam est earum fugiat repellendus officiis eveniet laudantium blanditiis porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quia. Exercitationem porro quaerat veritatis delectus atque autem eos esse eveniet ipsam minima illo dolore doloremque consectetur, rem quibusdam quo provident! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam impedit sunt aperiam amet adipisci odit asperiores porro, quod nulla tenetur tempore ducimus obcaecati ad rerum vitae architecto molestiae temporibus dolore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, voluptate. Dolore dolorum sunt repellendus! A vel, error eos iure, eligendi nam, iste maxime harum omnis cupiditate eaque ab! Perferendis, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem corrupti quo at exercitationem placeat quod nulla assumenda ipsam id nemo, expedita praesentium vel culpa facere nobis soluta, nihil necessitatibus dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione itaque cupiditate dignissimos rem pariatur consectetur consequatur voluptate nihil. Dolorem tempora ipsam error facere obcaecati nesciunt eligendi sit, at corrupti rerum! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quas esse nihil deserunt consequuntur? Quos vitae sapiente consequatur quisquam autem minima illo aliquam temporibus, provident nostrum ab iure, quibusdam repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quos fugit rerum cumque doloremque, delectus accusantium in distinctio illum aperiam accusamus vero sequi odit voluptates veniam provident saepe animi repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem officia iure porro eos velit. Ducimus repellendus, quibusdam tempore odit veniam, dolor ratione exercitationem tenetur aliquam nulla fugiat sunt, voluptatem deserunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum explicabo porro quas, assumenda nulla aliquid odio tempore, doloremque quisquam incidunt beatae, voluptas dicta commodi. At atque ut quibusdam unde veniam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laborum quasi corporis maiores iure mollitia asperiores recusandae magni eos, obcaecati labore, quos sit cupiditate optio impedit dolores molestiae ducimus pariatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, harum maiores iste molestiae odit dolor, facere neque, libero impedit sunt quisquam est earum fugiat repellendus officiis eveniet laudantium blanditiis porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quia. Exercitationem porro quaerat veritatis delectus atque autem eos esse eveniet ipsam minima illo dolore doloremque consectetur, rem quibusdam quo provident! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam impedit sunt aperiam amet adipisci odit asperiores porro, quod nulla tenetur tempore ducimus obcaecati ad rerum vitae architecto molestiae temporibus dolore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, voluptate. Dolore dolorum sunt repellendus! A vel, error eos iure, eligendi nam, iste maxime harum omnis cupiditate eaque ab! Perferendis, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem corrupti quo at exercitationem placeat quod nulla assumenda ipsam id nemo, expedita praesentium vel culpa facere nobis soluta, nihil necessitatibus dolores! </p>
+
+    </div>
+    <div class="secondsec">
+        <img class="secondimg" src="../assets/img/about/20096080_small-900x600.jpg" alt="">
+
+        <p class="thirdpar"> 'my silky poodle is so wet'</p>
+
+        <p class="forthpar"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero architecto, itaque optio culpa aut minima ea laudantium! Velit ipsum sint eius dolor quod reprehenderit maxime quisquam! Voluptates delectus enim incidunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Non labore ut architecto perspiciatis excepturi distinctio dolorem quas, fuga similique? Dolorem eos blanditiis id illo quas quaerat fugiat voluptatem ad deleniti? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam velit deleniti magni ullam aut voluptate doloribus modi quibusdam iste expedita voluptatem, ad perferendis sint excepturi facere distinctio quae, porro accusamus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum molestiae ducimus obcaecati, explicabo cum totam adipisci dignissimos enim assumenda! Tenetur rem nisi quod sequi officiis inventore tempora facilis quibusdam illo!</p>
+    </div>
 </div>
   </template>
   
   
-  <script>
-  
+
+  <script setup>
+ 
+</script>
 
   
-  </script>
+
   
   
   
   <style>
-  .card {
-      
-      width: 101vw;
-      height: 180px;
-      display: flex;
-      flex-wrap: wrap;
-      align-items: center;
-      border: 1px solid #C0FF3B;
-      margin-left: -8px;
-      margin-top: 20px;
-      transition: background-color 0.5s;
+.firstsec{
+
+margin-bottom: 100px;
+}
+
+.firstpar{
+    margin-left: 30px;
+  } 
+
+.secondpar{
+    margin-left: 30px;
+    margin-right: 30px;
   }
 
-  .card:hover{
-      background-color:#C0FF3B;;
-      opacity:1;
-      
-      
-    }
-.textDiv{
-          position:relative;
-          bottom:5px;
-          font-size: 100px;
-          /* per creare lo spessore nel testo*/
-          -webkit-text-stroke-width: 2.5px; /* Spessore del contorno */
-          -webkit-text-stroke-color:#C0FF3B;
-          color: transparent;
-          background-clip: text;
-          font-family: "Roboto", sans-serif;
-          font-weight:800;
-          margin-left: 80px;
-          transition:color 0.5s;
-      }     
-      
-      .card:hover .textDiv {
-        position:relative;
-          bottom:5px;
-          font-size: 100px;
-          /* per creare lo spessore nel testo*/
-          -webkit-text-stroke-width: 4px; /* Spessore del contorno */
-          -webkit-text-stroke-color: #05041D;
-          color: transparent;
-          background-clip: text;
-          font-family: "Roboto", sans-serif;
-          font-weight:800;
-          margin-left: 80px;
-          transition:color 0.5s;
+
+.thirdpar{
+  font-size: 80px;
+    /* per creare lo spessore nel testo*/
+    -webkit-text-stroke-width: 1px; /* Spessore del contorno */
+    -webkit-text-stroke-color:#C0FF3B;
+    color: transparent;
+    background-clip: text;
+    font-family: 'Consolas', sans-serif;
+    position: relative;
+    top:40px;
+    left: 100px;
+
 }
 
-.image {
-  width: 50px; /* Regola la larghezza dell'immagine come preferisci */
-  height: auto; /* Regola l'altezza dell'immagine come preferisci */
-  margin-right: 10px; /* Aggiungi margine destro per separare l'immagine dal testo */
+.forthpar {
+  position: relative; /* Imposta la posizione relativa */
+  top: 250px; /* Sposta il paragrafo verso il basso di 50 pixel (puoi regolare il valore come desideri) */
 }
 
+
+.firstimg{
+    display: flex;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+.secondimg {
+    float: left; /* Immagine a sinistra */
+  margin-right: 100px; /* Margine a destra per separare l'immagine dal paragrafo */
+  margin-left: 20px;
+} 
   </style>
