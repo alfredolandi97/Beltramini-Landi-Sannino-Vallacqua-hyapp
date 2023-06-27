63cfa9d70b26ad2473f88dc9d72050aaa8f54259
alfredolandi97 <alfredo.landi@mail.polimi.it>
2023-06-27 21:19:04 +0200

    Dynamic images added

--- pages/topprojects/index.vue
+++ pages/topprojects/index.vue
@@ -1,8 +1,7 @@
 <template>
     <p class="descrPag">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut sint eum minima. Eius illum qui quas reprehenderit dolorum earum corporis adipisci nemo optio beatae explicabo, id dicta sit a magnam!</p>
     <div id="card-containerPC">
-            <ProjectCard v-for = "topproject of topprojects" :title = "topproject.title" :area_id="topproject.area_id" :info="topproject.info" :link="'/topprojects/'+topproject.id"/>
-
+        <ProjectCard v-for = "topproject of topprojects" :title = "topproject.title" :area_id="topproject.area_id" :info="topproject.info" :link="'/topprojects/'+topproject.id" :logo="topproject.logo"/>
     </div>
     <p class="parEndPag"> You are on the Top Project page.</p>
 </template>
