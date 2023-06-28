29dcd93c51f19921d04f9f8843d97a649df9149d
alfredolandi97 <alfredo.landi@mail.polimi.it>
2023-06-28 14:50:32 +0200

    Dates fixed

--- server/api/news/[id].js
+++ server/api/news/[id].js
@@ -11,6 +11,16 @@ export default defineEventHandler(async (event) => {
         throw createError({statusCode: 400, statusMessage: error.message})
     }
 
+    const monthNames = ["January", "February", "March", "April",
+                        "May", "June", "July", "August",
+                        "September", "October", "November", "December"];
+
+    let date = new Date(data.date)
+    let day = date.getDate();
+    let month = monthNames[date.getMonth()]
+    let year = date.getFullYear(date);
+    data.date = `${day} ${month} ${year}`
+
     const image = await client.storage.from('next_fund').getPublicUrl(`news/${id}.png`)
     data.image = image.data.publicUrl
 
--- server/api/news/index.get.js
+++ server/api/news/index.get.js
@@ -9,7 +9,17 @@ export default defineEventHandler(async (event) => {
         throw createError({statusCode: 400, statusMessage: error.message})
     }
 
+    const monthNames = ["January", "February", "March", "April",
+                        "May", "June", "July", "August",
+                        "September", "October", "November", "December"];
+
     for(let update of data){
+        let date = new Date(update.date)
+        let day = date.getDate();
+        let month = monthNames[date.getMonth()]
+        let year = date.getFullYear(date);
+        update.date = `${day} ${month} ${year}`
+
         const image = await client.storage.from('next_fund').getPublicUrl(`news/${update.id}.png`)
         update.image = image.data.publicUrl
     }
